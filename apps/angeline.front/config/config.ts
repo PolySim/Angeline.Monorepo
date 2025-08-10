bda5daa56911317d330ce1cd8dfea29963eb5065
Simon Desdevises <simon.desdevises@gmail.com>
2025-08-11 00:48:06 +0200

    to next_public

--- apps/angeline.front/config/config.ts
+++ apps/angeline.front/config/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  API_URL: process.env.API_URL,
+  API_URL: process.env.NEXT_PUBLIC_API_URL,
   IMAGE_URL: process.env.NEXT_PUBLIC_IMAGE_URL,
   APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   BREVO_API_KEY: process.env.BREVO_API_KEY as string,
