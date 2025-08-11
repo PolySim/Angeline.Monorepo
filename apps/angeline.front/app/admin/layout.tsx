b3fc3c8cb1b32e6a3b33a4aa3e16b7f29303d895
Simon Desdevises <simon.desdevises@gmail.com>
2025-08-11 12:05:03 +0200

    add logout

---
+++ apps/angeline.front/app/admin/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { SignOutButton } from "@clerk/nextjs";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <div className="w-11/12 max-w-6xl mx-auto pb-2">
+        <Button asChild className="w-fit">
+          <SignOutButton>Déconnexion</SignOutButton>
+        </Button>
+      </div>
+      {children}
+    </>
+  );
+}
